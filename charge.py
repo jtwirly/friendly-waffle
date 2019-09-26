48699e2ff6af3fa11d91a6d3c6983bf32531d3c5
jtwirly <55814257+jtwirly@users.noreply.github.com>
2019-09-25 21:59:23 -0400

    Create charge.py

---
+++ charge.py
@@ -0,0 +1,21 @@
+# from https://github.com/stripe/stripe-python/blob/master/examples/charge.py
+
+from __future__ import absolute_import, division, print_function
+
+import os
+
+import stripe
+
+
+stripe.api_key = os.environ.get("STRIPE_SECRET_KEY")
+
+print("Attempting charge...")
+
+resp = stripe.Charge.create(
+    amount=200,
+    currency="usd",
+    card="tok_visa",
+    description="customer@gmail.com",
+)
+
+print("Success: %r" % (resp))
